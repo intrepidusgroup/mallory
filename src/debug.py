12cf71f08b14bb82810a0afb37215637b1982926
Jeremy Allen <jeremy.allen@intrepidusgroup.com>
2011-03-17 21:34:33 -0400

    Added documentation (epydoc) to the Debugger class. Performed some final cleanup and testing of the Debugger and how the GUI interacts with it.

--- src/debug.py
+++ src/debug.py
@@ -1,5 +1,3 @@
-from SimpleXMLRPCServer import SimpleXMLRPCServer
-# delete the SimpleXMLRPCServer import
 from observer import Subject
 import Pyro.core
 import Queue
@@ -38,13 +36,18 @@ class DebugEvent(object):
 #        return self
 
     def __str__(self):
-        s = "eventid=%s,event=%s,source=%s,destination=%s,data=%s,dir=%s,crc=%08x" % \
+        s = (("eventid=%s,event=%s,source=%s,destination=%s,data=%s,dir=%s,"
+             "crc=%08x") % 
         (self.eventid, self.event, self.source, self.destination, 
-         repr(self.data[:24]),self.direction, self.crc)
+         repr(self.data[:24]),self.direction, self.crc))
         return s
 
 
 class Debugger(Subject, Pyro.core.ObjBase):
+    """
+    The Debugger class is the primary remote interface for any remote stream
+    debugging clients. 
+    """
     def __init__(self, rules = []):
         Subject.__init__(self)
         Pyro.core.ObjBase.__init__(self)
@@ -55,6 +58,13 @@ class Debugger(Subject, Pyro.core.ObjBase):
         self.rules = rules
 
     def setdebug(self, state):
+        """
+        This method updates the state of debugging and notifies all observers,
+        typically TCP stream protocol handlers, that debugging should now be
+        performed.
+        
+        @param state: Boolean representing the debugging state
+        """
         self.log.debug("Debugger: self.debugon being set to: %s" % state)       
         if state:
             self.debugon = True
@@ -65,12 +75,20 @@ class Debugger(Subject, Pyro.core.ObjBase):
         return ""
     
     def getdebugq(self):
-        """Clear the debug queue and send them off to the client"""
+        """
+        Returns the debugging queue to the client. The queue is typically a 
+        list of debugging events. If the client never returns the event Mallory
+        will wait forever, eventually growing sad that the event has not been
+        returned. 
+        
+        @return: Array of L{debug.DebugEvent}
+        """
         debug_events = []
         while not self.debugq.empty():
             de = self.debugq.get()
-            self.log.debug("Debugger: got DebugEvent: %s:%s" % 
-                           (de.__class__,de))
+            
+            #self.log.debug("Debugger: got DebugEvent: %s:%s" % 
+            #               (de.__class__,de))
 
             debug_events.append(de)
             
@@ -81,6 +99,12 @@ class Debugger(Subject, Pyro.core.ObjBase):
         return debug_events       
         
     def update(self, publisher, **kwargs):
+        """
+        Handle notifications from other classes we are observing. Typically
+        this is a new event that needs to be placed into the debug queue.
+        
+        @param publisher: The object that sent the update
+        """
         #print "[*] Debugger: got update event. Not ignoring it."
         event = kwargs["event"]
                         
@@ -95,12 +119,24 @@ class Debugger(Subject, Pyro.core.ObjBase):
             de.direction = kwargs["direction"]
             de.crc = binascii.crc32(kwargs["thedata"])
         
-            self.log.debug("Debug.update: newevent: %08x [eventid:%s]" % (binascii.crc32(kwargs["thedata"]), kwargs["eventid"]))
+            self.log.debug("Debug.update: newevent: %08x [eventid:%s]" % 
+                           (binascii.crc32(kwargs["thedata"]), 
+                                            kwargs["eventid"]))
             
             self.debugq.put(de)
              
     def send_de(self, debugevent):
-        """Notify subscribers of an incoming debug event"""
+        """
+        Notify subscribers of an incoming debug event. It is easiest to 
+        understand this class name when it is thought of as a remote object
+        that a remote RPC client will be invoking. In that way the event
+        is "sent" and then we notify any of our observers it has come
+        in from the remote client. In this case the remote client will usually
+        be the Mallory GUI and the observer will be a TCPProtocl class managing
+        a TCP stream
+        
+        @param debugevent: The DebugEvent that the client is trying to send. 
+        """
         
         # Wrap it up nice and safe for transport to the debugger                
         de = debugevent
@@ -110,16 +146,24 @@ class Debugger(Subject, Pyro.core.ObjBase):
             self.log.error("Debugger: CRC MISMATCH: expecting %08x got %08x" %
                            (de.crc, localcrc))
             
-        self.log.debug("Debug.send_de: eventback: %08x [eventid:%s]" % (localcrc, debugevent.eventid))
+        self.log.debug("Debug.send_de: eventback: %08x [eventid:%s]" %
+                        (localcrc, debugevent.eventid))
         
         self.notify(event="debugevent",debugevent=de)
         
-        self.log.debug("Debug.send_de: Notify for [eventid:%s]" % (debugevent.eventid))
+        self.log.debug("Debug.send_de: Notify for [eventid:%s]" % 
+                       (debugevent.eventid))
+        
         #self.log.debug("Debugger: send_de: got debugevent %s: " %  (debugevent))
-#        self.log.debug("Debugger: send_de: got debugevent %s: " %  (de))
+        #self.log.debug("Debugger: send_de: got debugevent %s: " %  (de))
         return ""
     
-    def getrules(self):        
+    def getrules(self):
+        """
+        Get and return the current ruleset that is being enforced
+        
+        @return: array of L{rule.Rule} objects
+        """     
         if self.rules is None:
             return []
         
@@ -131,6 +175,11 @@ class Debugger(Subject, Pyro.core.ObjBase):
         return self.rules
     
     def updaterules(self, rulearray):
+        """
+        Update the current array of rule objects
+        
+        @param rulearray: The array of rule objects to replace the current one.
+        """
         self.rules = rulearray
         
         for rule in self.rules:
