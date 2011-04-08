5974fb09b5a6f07eb460b832ef07b014041bf504
Jeremy Allen <jeremy.allen@intrepidusgroup.com>
2011-04-07 21:57:07 -0400

    Added a database browser gui

---
+++ src/gui/DbGui.py
@@ -0,0 +1,119 @@
+from PyQt4 import QtGui, QtCore, Qt
+import TextViewDialog
+
+try:
+    from PyQt4 import QtSql
+    NoQtSql = False
+except:
+    NoQtSql = True
+    
+
+from protocol import http, sslproto
+
+
+FLOWS_QUERY = """
+select c.connCount, c.serverIp, c.serverPort, c.clientIp, c.clientPort, 
+    f.direction, f.timestamp, substr(buffer, 0, 32) as buffer
+from connections c
+inner join flows f ON f.conncount = c.conncount        
+"""
+        
+        
+class DbGui(object):
+    """
+    Quick and dirty database GUI
+    
+    This is an expert interface. But, it is here for whatever it is worth.
+    
+    """
+    def __init__(self, table_dbview, btn_exec_sql, btn_set_flows, text_db_sql,
+                 splitter_db):
+        self.table_dbview = table_dbview
+        self.btn_exec_sql = btn_exec_sql
+        self.btn_set_flows = btn_set_flows
+        self.text_db_sql = text_db_sql
+        self.splitter_db = splitter_db        
+        self.current_model = None
+        self.bufferview = BufferView(self)
+        
+        
+         # Set initial splitter sizes        
+        self.splitter_db.setSizes([200, 100])
+        
+        
+        self.connect_handlers()
+        self.db = None
+        
+        try:
+            self.db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
+            NoSqlite = False
+        except:
+            NoSqlite = True
+            
+        if NoQtSql or NoSqlite:
+            print ("You are missing QtSql (Ubuntu packagepython-qt4-sql) "
+                   "or QtSqlite (libqt4-sql-sqlite) and maybe both.") 
+            return
+        
+        self.db.setDatabaseName("../db/trafficdb")
+        open = self.db.open()
+        
+    def connect_handlers(self):
+        self.btn_exec_sql.clicked.connect(self.handle_exec_sql_clicked)
+        self.btn_set_flows.clicked.connect(self.handle_set_flows_clicked)
+        self.table_dbview.doubleClicked.connect(self.handle_row_double_clicked)
+            
+    def handle_exec_sql_clicked(self):
+        sql_str = self.text_db_sql.toPlainText()
+        model = QtSql.QSqlQueryModel()
+        model.setQuery(sql_str)
+        self.current_model = model
+        self.table_dbview.setModel(model)
+        
+    def handle_set_flows_clicked(self):
+        self.text_db_sql.setPlainText(FLOWS_QUERY)
+        
+    def handle_row_double_clicked(self, index):
+        row = index.row()
+        record = self.current_model.record(row)
+        conn_count = record.value("conncount").toInt()
+             
+        buffers = []   
+        #.toInt() returns a tuple
+        conn_count = conn_count[0]
+        query_str = "select buffer from flows where conncount = %d" % (conn_count)
+        query = QtSql.QSqlQuery(self.db)
+        query.exec_(query_str)
+        
+        while query.next():
+            buffers.append(query.record().value("buffer").toString())
+        
+        big_buffers_str = ""
+        for buffer in buffers:
+            big_buffers_str += buffer + "\n\n\n-----\n\n\n"
+
+        self.bufferview.setText(big_buffers_str)
+        self.bufferview.show()
+            
+            
+        
+        
+        
+class BufferView(QtGui.QDialog):
+    """
+    This class creates a dialog that is used to capture and then insert a Hex or
+    Text string into the data being edited.
+    """
+    def __init__(self, hexedit):
+        QtGui.QDialog.__init__(self)
+        self.dialog = TextViewDialog.Ui_Dialog()
+        self.dialog.setupUi(self)
+        
+    def setText(self, text):
+        self.dialog.textview.setPlainText(text)
+        
+
+        
+    
+
+
---
+++ src/gui/TextViewDialog.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'gui/TextViewDialog.ui'
+#
+# Created: Thu Apr  7 21:56:22 2011
+#      by: PyQt4 UI code generator 4.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(400, 300)
+        self.verticalLayout = QtGui.QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.textview = QtGui.QPlainTextEdit(Dialog)
+        self.textview.setObjectName("textview")
+        self.verticalLayout.addWidget(self.textview)
+        self.buttonBox = QtGui.QDialogButtonBox(Dialog)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Close)
+        self.buttonBox.setObjectName("buttonBox")
+        self.verticalLayout.addWidget(self.buttonBox)
+
+        self.retranslateUi(Dialog)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), Dialog.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
+
