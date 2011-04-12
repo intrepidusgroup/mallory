466005d4b7f42dbe569634802f390691559a2104
Jeremy Allen <jeremy.allen@intrepidusgroup.com>
2011-04-12 09:24:04 -0400

    Added compiled dialog UI file

---
+++ src/gui/AboutDialogUi.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'gui/AboutDialog.ui'
+#
+# Created: Mon Apr 11 23:26:35 2011
+#      by: PyQt4 UI code generator 4.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+class Ui_MainDialog(object):
+    def setupUi(self, MainDialog):
+        MainDialog.setObjectName("MainDialog")
+        MainDialog.resize(438, 583)
+        self.verticalLayout_2 = QtGui.QVBoxLayout(MainDialog)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtGui.QLabel(MainDialog)
+        self.label.setPixmap(QtGui.QPixmap(":/Other/icons/mall_logo1_medium.png"))
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.textEdit = QtGui.QTextEdit(MainDialog)
+        self.textEdit.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.textEdit.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.textEdit.setReadOnly(True)
+        self.textEdit.setObjectName("textEdit")
+        self.verticalLayout.addWidget(self.textEdit)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.retranslateUi(MainDialog)
+        QtCore.QMetaObject.connectSlotsByName(MainDialog)
+
+    def retranslateUi(self, MainDialog):
+        MainDialog.setWindowTitle(QtGui.QApplication.translate("MainDialog", "About Mallory", None, QtGui.QApplication.UnicodeUTF8))
+        MainDialog.setStyleSheet(QtGui.QApplication.translate("MainDialog", "#MainDialog {\n"
+"    background: rgb(255, 255, 255)\n"
+"}", None, QtGui.QApplication.UnicodeUTF8))
+        self.textEdit.setHtml(QtGui.QApplication.translate("MainDialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Mallory - Transparent MiTM Proxy</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><a href=\"http://intrepidusgroup.com/insight/mallory\"><span style=\" text-decoration: underline; color:#0000ff;\">http://intrepidusgroup.com/insight/mallory</span></a></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Copyright Intrepidus Group 2011. All rights reserved.</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">By: Jeremy Allen, Rajendra Umadas and ...</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Warning: Using Mallory may allow you to see a Double Rainbow (<img src=\":/icons/icons/rainbow.png\" /> <img src=\":/icons/icons/rainbow.png\" />)</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+
+import mallory_gui_rc
