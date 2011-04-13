6edf02edee694c147b37eec619d7597403c75852
Rajendra Umadas <raj.umadas@intrepidusgroup.com>
2011-04-13 10:52:36 -0400

    proper order for config for fuzzing

--- src/ruleconfig.py
+++ src/ruleconfig.py
@@ -28,7 +28,7 @@ userrules = [
 },
 {
  "name":"FuzzS2C",
- "action":rule.Fuzz(bof_injection_percentage=20*-1,bit_flip_percentage=45*-1), 
+ "action":rule.Fuzz(bit_flip_percentage=45*-1, bof_injection_percentage=20*-1), 
  "direction":"s2c",
  "passthru":"True"
 },
